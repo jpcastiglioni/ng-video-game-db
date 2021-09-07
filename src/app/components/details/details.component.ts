6c2014dce05a987396784196d10196599eb04093
Juan Pablo CD <jpcastiglioni@gmail.com>
2021-09-06 22:38:41 -0300

    Implementation to get details from http service, and to display them in view

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,20 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent implements OnInit {
-  gameRating= 0;
+export class DetailsComponent implements OnInit, OnDestroy {
+  gameRating = 0;
+  gameId!: string;
+  game!: Game;
+  routeSub!: Subscription;
+  gameSub!: Subscription;
 
-  constructor() { }
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private httpService: HttpService
+  ) {}
 
   ngOnInit(): void {
+    this.routeSub = this.ActivatedRoute.params.subscribe((params: Params) => {
+      this.gameId = params['id'];
+      this.getGameDetails(this.gameId);
+    });
   }
 
-  getColor(value: number):string {
-    if (value > 75 ) {
+  getGameDetails(id: string): void {
+    this.gameSub = this.httpService
+      .getGameDetails(id)
+      .subscribe((gameResp: Game) => {
+        this.game = gameResp;
+
+        setTimeout(() => {
+          this.gameRating = this.game.metacritic;
+        }, 1000);
+      });
+  }
+
+  getColor(value: number): string {
+    if (value > 75) {
       return '#5ee432';
     } else if (value > 50) {
       return '#fffa50';
@@ -25,4 +52,13 @@ export class DetailsComponent implements OnInit {
     }
   }
 
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
 }
